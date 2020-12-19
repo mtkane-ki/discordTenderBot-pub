0c647b03ab5cf6b5de2bb215a301042202ffc5dc
mtkane-ki <michael@kaneinformatics.io>
2020-12-19 14:24:04 -0500

    fix filter on second sub returned from api

--- pubStore.js
+++ pubStore.js
@@ -22,7 +22,7 @@ async function main(storeID) {
       return savings;
     }
   });
-
+  console.log(deliDeals);
   const subItems = deliDeals.filter(function (item) {
     if (item.title.toLowerCase().includes("sub")) {
       //console.log(item);
@@ -31,14 +31,19 @@ async function main(storeID) {
   });
 
   const saleSub = subItems.filter(function (subItem) {
-    if (subItem.description.toLowerCase().includes("fresh baked")) {
+    if (
+      !(
+        subItem.title.toLowerCase().includes("combo") ||
+        subItem.savings.toLowerCase().includes("combo")
+      )
+    ) {
       //console.log(subItem.title);
       return subItem;
     }
   });
   //console.log(saleSub)
   //const subTitle = saleSub[0].title
-  
+
   const trimmedRes = (({ title, description, finalPrice }) => ({
     title,
     description,
@@ -58,22 +63,23 @@ async function getStores(zipCode) {
     `storelocation?types=R,G,H,N,S&option=&count=15&includeOpenAndCloseDates=true&zipCode=${zipCode}`
   );
 
-  const top5Stores = res.data.Stores.slice(null, 5)
+  const top5Stores = res.data.Stores.slice(null, 5);
 
+  const trimmedStores = top5Stores.map(({ KEY, NAME, ADDR }) => ({
+    StoreNumber: KEY,
+    StoreName: NAME,
+    StoreAddress: ADDR,
+  }));
 
-  const trimmedStores = top5Stores.map(({KEY, NAME, ADDR}) => ({StoreNumber: KEY, StoreName: NAME, StoreAddress: ADDR}));
-
-
-  
-  const cleanedUpStores = trimmedStores.map( store => {
+  const cleanedUpStores = trimmedStores.map((store) => {
     store.StoreNumber = Number(store.StoreNumber); //trim leading zeroes by converting to number type
     store.StoreNumber = String(store.StoreNumber); //convert back to string for later text display to end user
-    return store
-  })
-  
+    return store;
+  });
+
   //console.log(cleanedUpStores)
 
-  return cleanedUpStores
+  return cleanedUpStores;
 }
 
 function queryStore(storeNum) {
